59b833fd9880320856aed59c2fd79f87f28544a5
Matthew Gallant <mgallant@aptuitiv.com>
2022-05-12 16:25:23 -0400

    Fix main.js start syntax

--- generators/app/templates/src/js/main.js
+++ generators/app/templates/src/js/main.js
@@ -6,18 +6,20 @@
 
 // Initialization
 document.onreadystatechange = function () {
-    smallScreenNav.init();
-    navAccess.init();
+    if (document.readyState !== 'loading') {
+        smallScreenNav.init();
+        navAccess.init();
 
-    // Back to top link handler
-    const link = document.querySelector('.js-btop');
-    link.addEventListener('click', (e) => {
-        e.preventDefault();
-        e.stopPropagation();
-        window.scrollTo({
-            top: 0,
-            left: 0,
-            behavior: 'smooth',
+        // Back to top link handler
+        const link = document.querySelector('.js-btop');
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            window.scrollTo({
+                top: 0,
+                left: 0,
+                behavior: 'smooth',
+            });
         });
-    });
+    }
 };
